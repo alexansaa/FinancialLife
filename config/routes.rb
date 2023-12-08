e64e7446331fdda76b174b809e7a5769bc221a28
alexansaa <alexan.saa@gmail.com>
2023-12-08 09:04:04 -0500

    deployment configurations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'health/check'
   devise_for :users, controllers: { registrations: 'registrations' }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   resources :categories, only: [:index, :new, :create, :show]
   resources :transactions, only: [:new, :create]
   
+  get 'health/check', to: 'health#check'
 
   # Defines the root path route ("/")
   # root "posts#index"
