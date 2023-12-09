6431287b8f9dd10be53160f08f14f9c7cf6ae86d
alexansaa <alexan.saa@gmail.com>
2023-12-09 09:32:33 -0500

    styling 30%

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,12 +6,4 @@ class RegistrationsController < Devise::RegistrationsController
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
-
-  # def sign_up_params
-  #   params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  # end
-
-  # def account_update_params
-  #   params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
-  # end
 end
